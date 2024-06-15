ad2bd596171b11709d5e7e91d3ab1735f1267f5d
niharika0706 <niharika0706@gmail.com>
2024-06-14 21:35:35 -0400

    Firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <h2>App Component</h2>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h2 style={{ color: "green" }}>Header Component</h2>
+    </div>
+  );
+}
+
+export default Header;
